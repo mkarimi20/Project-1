ee109a87db4722fe6c9c85721577b6cf2d269eff
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-16 15:27:46 -0600

    ect variables

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-us-burak"
+    bucket = "acirrustech-env-prod"
     key    = "aws/ec2/ec2_state"
     region = "us-east-1"
   }
--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "prod-instance" {
   count = var.count_instance
   ami             = var.ami
   instance_type   = var.instance_type
   associate_public_ip_address = var.associate_public_ip_address
-  key_name = aws_key_pair.deployer.key_name
-  security_groups = ["allow_ssh"]
+  key_name = aws_key_pair.mkarimi.key_name
+  security_groups = ["bastion-allow"]
   provisioner "remote-exec" {
     connection {
       host = self.public_ip
@@ -26,6 +26,6 @@ resource "aws_instance" "web" {
     prevent_destroy = false
   }
   tags = {
-    Name = "HelloWorld${count.index +1}"
+    Name = "prod${count.index +1}"
   }
 }
