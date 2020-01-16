5889da3069372c91ec5d383068114c5f311b75de
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-16 17:01:59 -0600

    sg added

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "acirrustech-env-prod"
-    key    = "infrustructure"
+    key    = "infrustructure/prod"
     region = "eu-west-1"
   }
 }
---
+++ sec-group.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "bastion-allow" { 
+  name        = "bastion-allow" 
+  description = "Allow TLS inbound traffic" 
+  vpc_id      = "vpc-908caaf6" 
+
+  ingress { 
+    from_port   = 22
+    to_port     = 22 
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]   
+
+} 
+
+    ingress { 
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"]   
+
+} 
+} 
