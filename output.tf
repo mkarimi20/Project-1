7a32b27dcbf675ceb98e3a6cec84fb74981046e7
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-16 16:41:52 -0600

    backend added

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "bucket_name"  {
+value = aws_s3_bucket.b.bucket
+}
+
+output "bucket_name2"  {
+value = aws_security_group.allow_ssh.name
+}
+
+output "key_name"  {
+value = aws_key_pair.deployer.key_name
+}
+
+output "IPs3" {
+  value = aws_instance.web.*.public_ip
+}
