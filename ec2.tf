b61b01b101982f3b3dabd099dcf258bbf3a3a145
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-16 14:16:38 -0600

    additional information added for ec2 - pending

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,31 @@
-resource "aws_instance" "instance" {
-    ami= var.ami
-    instance_type = var.instance_type
-    key_name = var.key_name
-    tags = {
-        Name = var.environment
-    }
+resource "aws_instance" "web" {
+  count = var.count_instance
+  ami             = var.ami
+  instance_type   = var.instance_type
+  associate_public_ip_address = var.associate_public_ip_address
+  key_name = aws_key_pair.deployer.key_name
+  security_groups = ["allow_ssh"]
+  provisioner "remote-exec" {
+    connection {
+      host = self.public_ip
+      type = "ssh"
+      user = var.user
+      private_key = file(var.ssh_key_location)
+      }
+      inline = [
+        "sudo yum install -y epel-release",
+        "sudo yum install httpd -y ",
+        "sudo systemctl start httpd",
+        "sudo systemctl enable httpd",
+        ]
+      }
+  provisioner "local-exec" {
+    command = "echo Hello >> /tmp/file_hello"
+  }
+  lifecycle{
+    prevent_destroy = false
+  }
+  tags = {
+    Name = "HelloWorld${count.index +1}"
+  }
 }
--- variable.tf
+++ variable.tf
@@ -1,9 +1,6 @@
 variable "ami" {}
-variable "key_name" {}
 variable "instance_type" {}
-variable "environment" {} 
-variable "s3_bucket" {} 
-variable "s3_folder_project" {} 
-variable "s3_folder_region" {} 
-variable "s3_folder_type" {} 
-variable "s3_tfstate_file" {}
+variable "count_instance"{}
+variable "associate_public_ip_address"{}
+variable "user"{}
+variable "ssh_key_location"{}
