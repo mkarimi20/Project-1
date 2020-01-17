fcc2e0d3c02cc39254381693cf0ba4822faf9280
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-17 13:29:51 -0600

    variables updated

--- variable.tf
+++ variable.tf
@@ -2,10 +2,10 @@
 variable "instance_type" {}
 variable "count_instance"{}
 variable "associate_public_ip_address"{}
-#variable "user"{}
-#variable "ssh_key_location"{}
-#variable "key_name" {}
-#variable "security_group" {}
+variable "user"{}
+variable "ssh_key_location"{}
+variable "key_name" {}
+variable "security_group" {}
 variable "region" {
   
 }
