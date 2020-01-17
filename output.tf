26d6d7a806642defd92fd9d9e1231d6ca6f1d6f6
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-17 13:58:29 -0600

    output modified

--- output.tf
+++ output.tf
@@ -6,11 +6,11 @@ output "bucket_name2"  {
 value = aws_security_group.bastionhost.name
 }
 output "sec_group"  {
-value = aws_s3_bucket..bucket
+value = aws_s3_bucket.cirrustech-env-prod.bucket
 }
 
 output "key_name"  {
-value = aws_key_pair.deployer.key_name
+value = aws_key_pair.terraform.key_name
 }
 
 output "IPs3" {
